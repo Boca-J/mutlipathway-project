d3b2073f4b4da950e8da0bedbf54bdceda30931e
Boca-J <1318029308reng@gmail.com>
2024-04-02 22:11:06 -0400

    bug not fixed

--- analysis.py
+++ analysis.py
@@ -15,12 +15,25 @@ from torch.utils.data import DataLoader, TensorDataset
 from multichannel_net import MultipathwayNet
 
 
-from multichannel_net import MultipathwayNet
-torch.manual_seed(5)
+def plot_kakb(ka, kb):
+    fig, axes = plt.subplots(nrows=1, ncols=2)
+    ka = ka.T.detach().numpy()
+    kb = kb.T.detach().numpy()
+    # Plot the first heatmap
+    heatmap1 = axes[0].imshow(ka, cmap='hot', interpolation='nearest')
+    axes[0].set_title('Ka')  # Title for the first heatmap
+    fig.colorbar(heatmap1, ax=axes[0])  # Add a colorbar for the first heatmap
+
+    # Plot the second heatmap
+    heatmap2 = axes[1].imshow(kb, cmap='hot', interpolation='nearest')
+    axes[1].set_title('Kb')  # Title for the second heatmap
+    fig.colorbar(heatmap2, ax=axes[1])  # Add a colorbar for the second heatmap
+
+    # Show the plot
+    plt.show()
 
 
-
-
+# X is a 8x8 identity matrix
 X_default = torch.eye(8)
 Y_default = torch.Tensor([[1, 1, 1, 1, 1, 1, 1, 1],
                           [1, 1, 1, 1, 0, 0, 0, 0],
@@ -41,8 +54,6 @@ Y_default = torch.Tensor([[1, 1, 1, 1, 1, 1, 1, 1],
                           [0, 0, 0, 0, 0, 0, 0, 1],
                           ]).T
 
-
-
 Y_alt = torch.Tensor([[0, 0, 0, 0, 0, 1, 1, 1],
                       [0, 1, 1, 1, 0, 1, 0, 1],
                       [0, 0, 1, 0, 1, 0, 1, 0],
@@ -60,13 +71,23 @@ Y_alt = torch.Tensor([[0, 0, 0, 0, 0, 1, 1, 1],
                       [0, 0, 1, 1, 1, 0, 0, 1]]).T
 
 
+def random_noise_addition(x, mean=0.01, std=0.003):
+    noise = torch.randn_like(x) * std + mean
+    noisy_x = x + noise
+    return noisy_x
+
+
 class MPNAnalysis(object):
     def __init__(self, mcn, X=X_default, Y=Y_default, device=None):
 
         self.mcn = mcn
+        self.encoder1 = mcn.copy()
+        self.encoder2 = mcn.copy()
 
         self.X = X
         self.Y = Y
+
+        # self.scl_optimizer = optim.Adam(self.encoder.parameters(), lr=1e-3)
 
         if device is None:
             if torch.backends.mps.is_built():
@@ -84,11 +105,13 @@ class MPNAnalysis(object):
         # self.X = self.X.to(self.device)
         # self.Y = self.Y.to(self.device)
 
+        # matrix multiplication of the transpose of self.Y with self.X,
+        # divided by the number of rows in self.Y.
+
+        # todo uncomment this when doing the old training
         sigma_yx = self.Y.T.mm(self.X) / self.Y.shape[0]
 
         U, S, V = torch.svd(sigma_yx, some=False)
-
-
 
         self.U = U
         self.S = S
@@ -98,129 +121,283 @@ class MPNAnalysis(object):
         self.omega_history = None
         self.K_history = None
 
+    # return k = U*omega*V
     def omega2K(self, omega):
 
+        #  make certain operations more efficient by skipping gradient computations
         with torch.no_grad():
             k = omega.mm(self.V)
             k = self.U.T.mm(k)
 
         return k
 
+    def nt_xent_loss(self, z_i, z_j, temperature=0.5):
+        # Normalize the embeddings
+        z_i_norm = F.normalize(z_i, p=2, dim=1)
+        z_j_norm = F.normalize(z_j, p=2, dim=1)
+
+        # Concatenate the normalized embeddings
+        z = torch.cat((z_i_norm, z_j_norm), dim=0)
+
+        # Compute similarity matrix
+        sim_matrix = torch.matmul(z, z.T) / temperature
+        sim_matrix.fill_diagonal_(
+            -9e15)  # Fill diagonal with very small numbers to exclude self-similarity
+
+        # Create labels for 2N examples, where each example's label is its positive pair index
+
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        labels = torch.arange(z.size(0), device=device)
+        labels = (labels + z.size(0) // 2) % z.size(0)
+
+        # Compute NT-Xent loss (i.e., normalized temperature-scaled cross entropy loss)
+        loss_fct = torch.nn.CrossEntropyLoss()
+        loss = loss_fct(sim_matrix, labels)
+
+        return loss
+
+    # oringinal train function
+    # def train_mcn(self, timesteps=1000, lr=0.01):
+    #
+    #     # squared error loss function
+    #     loss = torch.nn.MSELoss(reduction='sum')
+    #
+    #     # stochastic gradient descent (SGD) optimizer (torch.optim.SGD)
+    #     # to update the parameters of the neural network (self.mcn) during training.
+    #     # The learning rate (lr) is set to the provided value.
+    #     optimizer = torch.optim.SGD(params=self.mcn.parameters() , lr=lr)
+    #
+    #     # just set the model to train mode and do nothign else,
+    #     # mcn is the model
+    #     self.mcn.train()
+    #
+    #     loss_history = []
+    #     omega_history = []
+    #
+    #     for t in range(10):
+    #         #  model make predictions (output) based on input X.
+    #         output = self.mcn(self.X)
+    #
+    #
+    #         loss_val = loss(output, self.Y)
+    #
+    #         loss_history.append(loss_val.to("cpu").detach().numpy())
+    #
+    #         omega_history.append(self.mcn.omega())
+    #
+    #         # computes the gradients of the loss with respect to all the model's parameters.
+    #         loss_val.backward()
+    #
+    #         # use sgd to update the model's parameters (weights and biases) based on these gradients.
+    #         optimizer.step()
+
+    #         optimizer.zero_grad()
+    #
+    #
+    #     omega_history = zip(*omega_history)
+    #
+    #
+    #
+    #     # convert omegas to Ks
+    #     K_history = []
+    #     for pathway in omega_history:
+    #         # om is the layer in pathway
+    #         K_history.append([self.omega2K(om) for om in pathway])
+    #
+    #
+    #     # print(len(K_history))
+    #
+    #     exit()
+    #     self.loss_history = loss_history
+    #     self.omega_history = omega_history
+    #     self.K_history = K_history
+    #
+    #     return loss_history, K_history
+
+    # simclr train function
+    ############################################################
+    # def train_mcn(self, timesteps=1000, lr=0.01):
+    #
+    #     loss_history = []
+    #     omega_history = []
+    #
+    #     self.encoder1.train()
+    #     self.encoder2.train()
+    #
+    #     optimizer1 = torch.optim.SGD(params=self.encoder1.parameters(), lr=lr)
+    #     optimizer2 = torch.optim.SGD(params=self.encoder2.parameters(), lr=lr)
+    #
+    #
+    #
+    #     for t in range(300):
+    #
+    #
+    #
+    #         # Apply augmentations to create a pair
+    #         batch_augmented_1 = random_noise_addition(self.X)
+    #         batch_augmented_2 = random_noise_addition(self.X)
+    #
+    #
+    #         # Compute representations
+    #         z_i = self.encoder1(batch_augmented_1) + self.encoder2(batch_augmented_1)
+    #         z_j = self.encoder1(batch_augmented_2) + self.encoder2(batch_augmented_2)
+    #
+    #
+    #
+    #         # Compute contrastive loss
+    #         loss_val = self.nt_xent_loss(z_i, z_j)
+    #
+    #         loss_history.append(loss_val.to("cpu").detach().numpy())
+    #
+    #
+    #         # this is to make the omega list same format as the orignial one, so that
+    #         # it can work with the plot functions later
+    #         omega = self.encoder1.omega()
+    #
+    #         for matrix in self.encoder2.omega():
+    #             omega.append(matrix)
+    #
+    #
+    #         # omega_history.append(omega)
+    #         omega_history.append([self.encoder1(self.X), self.encoder2(self.X)])
+    #
+    #         loss_val.backward()
+    #         optimizer1.step()
+    #         optimizer2.step()
+    #
+    #         optimizer1.zero_grad()
+    #         optimizer2.zero_grad()
+    #
+    #
+    #
+    #
+    #
+    #     history = zip(*omega_history)
+    #
+    #
+    #     # sig
+    #     print(self.encoder2.omega())
+    #     exit()
+    #
+    #     mat1 = self.encoder1(self.X)
+    #     mat2 = self.encoder2(self.X)
+    #     ouptut_matrix = mat1+mat2
+    #     #ouptut_matrix = ouptut_matrix.T
+    #
+    #     # U, S, V = torch.svd(mat1, some=False)
+    #     U, S, V = torch.svd(ouptut_matrix, some=False)
+    #
+    #     self.U = U
+    #     self.S = S
+    #     self.V = V
+    #
+    #     ka = self.omega2K(mat1)
+    #     kb = self.omega2K(mat2)
+    #
+    #
+    #
+    #
+    #
+    #
+    #     K_history = []
+    #
+    #     for pathway in history:
+    #         # om is the layer in pathway
+    #
+    #         K_history.append([self.omega2K(om).T for om in pathway])
+    #
+    #     self.loss_history = loss_history
+    #     self.omega_history = omega_history
+    #     self.K_history = K_history
+    #
+    #     return loss_history, K_history
+
+    #######################################################
+    # two sigma:
+
     def train_mcn(self, timesteps=1000, lr=0.01):
-        torch.manual_seed(5)
-        if torch.cuda.is_available():
-            torch.cuda.manual_seed_all(5)
 
-        diag_values = torch.rand(8) + 0.5
+        sigma_yx1 = self.Y.T.mm(self.X) / self.Y.shape[0]
+        num_features = sigma_yx1.shape[1]  # Number of features in X
+        num_labels = sigma_yx1.shape[0]  # Number of labels in Y
+        num_samples = 8
 
-        mat1 = torch.zeros((8, 15))
-        mat1[range(8), range(8)] = diag_values
+        # generate X and y using non-negative
+        torch.manual_seed(0)
 
-        diag_values = torch.rand(8) + 1
+        sigma_yx_new = torch.rand((15, 8))
+        sigma_yx_new *= self.Y.shape[0]
+        n_components = 8  # For example, let's factorize sigma_yx_new into 4 components
 
-        mat2 = torch.zeros((8, 15))
-        mat2[range(8), range(8)] = diag_values
+        model = NMF(n_components=n_components, init='random', random_state=0,
+                    max_iter=500)
+        W = model.fit_transform(sigma_yx_new)
+        H = model.components_
 
-        x1 = torch.rand((8, 8))
-        x2 = torch.rand((8, 8))
+        Y_new = torch.tensor(W.T, dtype=torch.float)
+        X_new = torch.tensor(H.T, dtype=torch.float)
 
-        y1 = x1.mm(mat1)
-        y2 = x2.mm(mat2)
+        X = torch.cat([self.X, X_new],
+                      dim=0)  # Concatenate along the first axis (rows)
+        Y = torch.cat([self.Y, Y_new], dim=0)
 
-        # self.X = x1
-        # self.Y = x1.mm(mat1)
+        dataset = TensorDataset(X, Y)
 
-        # self.X = x2
-        # self.Y = x2.mm(mat2)
+        # Step 3: Create a DataLoader
+        data_loader = DataLoader(dataset, batch_size=8, shuffle=True)
 
-        self.X = torch.cat([x1, x2],dim=0)
-        self.Y = torch.cat([y1, y2], dim=0)
-
-        sigma_yx = self.Y.T.mm(self.X) / self.Y.shape[0]
-
-        # projection to the x1y1 sigma
-        sigma_yx = y1.T.mm(x1) / y1.shape[0]
-
-        #  mat2 coordinates projection
-        # sigma_yx = y2.T.mm(x2) / y2.shape[0]
-
-
-        U, S, V = torch.svd(sigma_yx, some=False)
-
-        self.U = U
-        self.S = S
-        self.V = V
-
-        #
-        # # identity_matrix = torch.eye(15)
-        # #
-        # #
-        # # selected_columns = np.array([1,7,5,2,14,6,9,11])
-        # # x1 = identity_matrix[:, selected_columns]
-        #
-        # # Step 3: Determine the columns not selected for x1
-        #
-        #
-        # y1 = x1.mm(mat1)
-        # print(y1)
-        #
-        # y2 = x2.mm(mat2)
-
-
-        # y2 *= y2.shape[0]
-
-
-
-
-
-
-
-        # self.X = torch.cat([x1, x2],dim=0)
-        # self.Y = torch.cat([y1, y2], dim=0)
-
-        # self.X = x1
-        # self.Y = y1
-
-        # sigma_yx = self.Y.T.mm(self.X) / self.Y.shape[0]
-        #
-        # U, S, V = torch.svd(sigma_yx, some=False)
-        # #
-        # # U, S, V = torch.svd(sigma_yx1, some=False)
-        # #
-        # #
-        # self.U = U
-        # self.S = S
-        # self.V = V
-
-
+        # squared error loss function
         loss = torch.nn.MSELoss(reduction='sum')
+
+        # stochastic gradient descent (SGD) optimizer (torch.optim.SGD)
+        # to update the parameters of the neural network (self.mcn) during training.
+        # The learning rate (lr) is set to the provided value.
         optimizer = torch.optim.SGD(params=self.mcn.parameters(), lr=lr)
 
+        # just set the model to train mode and do nothign else,
+        # mcn is the model
         self.mcn.train()
 
         loss_history = []
         omega_history = []
 
         for t in range(timesteps):
-            output = self.mcn(self.X)
-            loss_val = loss(output, self.Y)
+            #  model make predictions (output) based on input X.
+            output = self.mcn(X_new)
+
+            loss_val = loss(output, Y_new)
 
             loss_history.append(loss_val.to("cpu").detach().numpy())
+
             omega_history.append(self.mcn.omega())
 
+            # computes the gradients of the loss with respect to all the model's parameters.
             loss_val.backward()
+
+            # use sgd to update the model's parameters (weights and biases) based on these gradients.
             optimizer.step()
 
             optimizer.zero_grad()
 
-
-
         omega_history = zip(*omega_history)
+
+        sigma_yx_new = Y_new.T.mm(X_new) / Y_new.shape[0]
+        U, S, V = torch.svd(sigma_yx_new, some=False)
+        self.U = U
+        self.V = V
+        #
+        # sigma =  Y.T.mm(X) / Y.shape[0]
+        # U, S, V = torch.svd(sigma, some=False)
+        # self.U = U
+        # self.V = V
 
         # convert omegas to Ks
         K_history = []
         for pathway in omega_history:
+            # om is the layer in pathway
             K_history.append([self.omega2K(om) for om in pathway])
+
+        # print(len(K_history))
 
         self.loss_history = loss_history
         self.omega_history = omega_history
@@ -228,6 +405,7 @@ class MPNAnalysis(object):
 
         return loss_history, K_history
 
+    # all the code below are from the old code
     def plot_K(self, ax, savedir='', labels=None, savename=None, savelabel='',
                min_val=0, max_val=2):
 
@@ -251,7 +429,7 @@ class MPNAnalysis(object):
             ax[i].set_title(r'$\bf K_{}$'.format(labels[i]), fontsize=20)
             ax[i].axis('off')
 
-        plt.colorbar(im, ax=ax, shrink=0.6)
+        plt.colorbar(im, ax=ax, shrink=1)
 
     def plot_K_history(self, ax, savename=None, D='unknown', savelabel=''):
 
@@ -287,6 +465,7 @@ class MPNAnalysis(object):
         ax.legend(fontsize=17, loc='upper left')
 
         ax.set_box_aspect((2.25, 1.75, 1))
+        return
 
 
 if __name__ == '__main__':
@@ -316,7 +495,7 @@ if __name__ == '__main__':
 
     # timesteps = args.timesteps
 
-    depth_list = [2, 3, 4, 7]
+    depth_list = [7, 2]
 
     fig_train, ax_train = plt.subplots(1, len(depth_list), figsize=(25, 8))
     ax_train[0].set_ylabel('training error')
@@ -328,13 +507,9 @@ if __name__ == '__main__':
     timestep_list = [1000, 1000, 1400, 10000]
 
     min_val = 0.0
-    max_val = 1.0
+    max_val = 0.00001
 
-    i = 0
     for di, depth in enumerate(depth_list):
-        if i == 1:
-            break
-        i += 1
         ax3d = fig_history.add_subplot(gs[di * 3], projection='3d')
         ax2 = fig_history.add_subplot(gs[di * 3 + 1])
         ax3 = fig_history.add_subplot(gs[di * 3 + 2])
@@ -367,3 +542,4 @@ if __name__ == '__main__':
     fig_history.savefig('test.pdf')
 
     plt.show()
+
--- multichannel_net.py
+++ multichannel_net.py
@@ -1,74 +1,123 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
-
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 
 class MultipathwayNet(torch.nn.Module):
-    def __init__(self, input_dim, output_dim, nonlinearity=None, bias=False,
-                 num_pathways=2, depth=2, width=1000, eps=0.01, hidden=None):
+    def __init__(self, input_dim, output_dim, nonlinearity=None, bias=False, num_pathways=2, depth=2, width=1000, eps=0.01, hidden=None):
 
         super(MultipathwayNet, self).__init__()
 
         # hidden is assumed to be a list with entries corresponding to each pathway, each entry a list of the widths of that pathway by depth
         # hidden!=None will override num_pathways, depth, width
-        # 'depth' above is the number of weights, not the number of hidden layers
+
 
         self.input_dim = input_dim
         self.output_dim = output_dim
         self.bias = bias
         self.nonlinearity = nonlinearity
 
+        # used for deep copu function
+        self.depth = depth
+        self.width = width
         self.eps = eps
+
+        hidden_size = output_dim
+        projection_size = output_dim
+        self.projection_head = nn.Sequential(
+            nn.Linear(output_dim,hidden_size ),
+            nn.BatchNorm1d(hidden_size),
+            nn.Linear(hidden_size, projection_size)
+        )
 
         if hidden is None:
             hidden = []
             for pi in range(num_pathways):
                 pathway = []
-                for di in range(depth - 1):
+                for di in range(depth-1):
                     pathway.append(width)
                 hidden.append(pathway)
+        # hidden first layer is path way, each element is a list of length depth and each entry in this list is width
 
         self.hidden = hidden
 
         self.hidden_layers = []
 
+
+
+        # iterate through each pathway
         for pathway in self.hidden:
+
+            # the num indicator ensure that we only create one pathway so that we can
+            # use the mcn class as the encoder
+
+
             op_list = []
+
+            # the depth here is the number of neurons (width)
             for di, depth in enumerate(pathway):
-                if di == 0:
+                if di==0:
+                    # depth here is the dimension of output vecotr
                     op = torch.nn.Linear(self.input_dim, depth, bias=self.bias)
+
+                    # if concat modify here
 
 
                 else:
-                    op = torch.nn.Linear(pathway[di - 1], depth,
-                                         bias=self.bias)
+                    # input dim is the width of last layer
+                    op = torch.nn.Linear(pathway[di-1], depth, bias=self.bias)
 
-                op.weight = torch.nn.Parameter(
-                    torch.randn(op.weight.shape) * eps, requires_grad=True)
+                    #  initializes its weight with random values scaled by eps and sets it to require gradients for training
+                op.weight = torch.nn.Parameter(torch.randn(op.weight.shape)*eps, requires_grad=True)
 
+                # If the linear layer has a bias (i.e., op.bias is not None), it initializes the bias with zeros and sets it to require gradients for training.
                 if op.bias is not None:
-                    op.bias = torch.nn.Parameter(torch.zeros_like(op.bias),
-                                                 requires_grad=True)
+                    op.bias = torch.nn.Parameter(torch.zeros_like(op.bias), requires_grad=True)
                 op_list.append(op)
 
+
+            #  creates a final linear layer that connects the last hidden layer to the output layer.
+            # since the depth in hidden is the nunmber of hidden layers, not including the last layer
             op = torch.nn.Linear(pathway[-1], self.output_dim, bias=self.bias)
-            op.weight = torch.nn.Parameter(torch.randn(op.weight.shape) * eps,
-                                           requires_grad=True)
+            op.weight = torch.nn.Parameter(torch.randn(op.weight.shape)*eps, requires_grad=True)
+
+
+
             if op.bias is not None:
-                op.bias = torch.nn.Parameter(torch.zeros_like(op.bias),
-                                             requires_grad=True)
+                op.bias = torch.nn.Parameter(torch.zeros_like(op.bias), requires_grad=True)
             op_list.append(op)
+
 
             self.hidden_layers.append(op_list)
 
+            # do a deep copy
+            # else:
+            #     op_list = []
+            #     for first_layer in first_pathway_layers:
+            #         new_layer = torch.nn.Linear(first_layer.in_features,
+            #                                     first_layer.out_features,
+            #                                     bias=self.bias)
+            #         new_layer.weight = torch.nn.Parameter(
+            #             first_layer.weight.clone().detach(),
+            #             requires_grad=True)
+            #         if self.bias:
+            #             new_layer.bias = torch.nn.Parameter(
+            #                 first_layer.bias.clone().detach(),
+            #                 requires_grad=True)
+            #         op_list.append(new_layer)
+            #         # Add the new pathway with the deep copied layers
+            #     self.hidden_layers.append(op_list)
+
+
+        # store name for each paramenter. useful in model saving and loading, accessing and updating specific parameters during training
         for pi, op_list in enumerate(self.hidden_layers):
             for oi, op in enumerate(op_list):
-                self.register_parameter(
-                    name="Path_{}_Depth_{}_weight".format(pi, oi),
-                    param=op.weight)
-                self.register_parameter(
-                    name="Path_{}_Depth_{}_bias".format(pi, oi), param=op.bias)
+                self.register_parameter(name= "Path_{}_Depth_{}_weight".format(pi, oi), param=op.weight)
+                self.register_parameter(name= "Path_{}_Depth_{}_bias".format(pi, oi), param=op.bias)
 
+        # inserts the activation function immediately after each linear layer in the pathway.
         if self.nonlinearity is not None:
             temp_layers = self.hidden_layers
             self.hidden_layers = []
@@ -79,35 +128,91 @@ class MultipathwayNet(torch.nn.Module):
                     new_op_list.append(self.nonlinearity)
                 self.hidden_layers.append(new_op_list)
 
+
+    # first forward a single pathway, then forward another pathway, and add the result together
     def forward(self, x):
 
+        # adding 0 to a tensor means adding 0 on every element
         output = 0
+
 
         for op_list in self.hidden_layers:
             xtemp = x
+
+            # op is a linear layer created by porch
             for op in op_list:
                 xtemp = op(xtemp)
+
+            # here output got added in each pathway
+
             output += xtemp
 
+
+
+        # output = self.projection_head(output)
         return output
 
-    def omega(self):
+    # a deep copy function ( without copying the hidden layer)
+    def copy(self):
+
+
+        new_net = MultipathwayNet(
+            input_dim=self.input_dim,
+            output_dim=self.output_dim,
+            nonlinearity=self.nonlinearity,
+            bias=self.bias,
+            num_pathways=len(self.hidden),
+            depth=self.depth,
+            # Assuming depth is the number of layers
+            width=self.width,
+            # Assuming width is the size of the first layer
+            eps=self.eps,
+            hidden=self.hidden.copy()
+        )
+        for pi, pathway in enumerate(self.hidden_layers):
+            for di, layer in enumerate(pathway):
+                # Copy the weights
+                with torch.no_grad():
+                    new_net.hidden_layers[pi][di].weight = torch.nn.Parameter(
+                        layer.weight.clone())
+
+                    # If the layer has a bias, copy it
+                    if layer.bias is not None:
+                        new_net.hidden_layers[pi][
+                            di].bias = torch.nn.Parameter(layer.bias.clone())
 
         with torch.no_grad():
-            x = torch.eye(self.input_dim).to(
-                self.hidden_layers[0][0].weight.device)
+            for layer, new_layer in zip(self.projection_head,
+                                        new_net.projection_head):
+                if hasattr(layer, 'weight'):
+                    new_layer.weight = torch.nn.Parameter(layer.weight.clone())
+                if hasattr(layer, 'bias') and layer.bias is not None:
+                    new_layer.bias = torch.nn.Parameter(layer.bias.clone())
 
+        return new_net
+
+
+
+    #  omega is the sum of all weight matrics? Y = OmegaX
+
+    def omega(self):
+        # no gradients are calculated during the execution of the code inside the block
+        with torch.no_grad():
+            # creates an identity matrix x with dimensions determined by self.input_dim
+
+            x = torch.eye(self.input_dim).to(self.hidden_layers[0][0].weight.device)
             output = []
 
-            # identity_matrix = torch.eye(15)
-            # selected_columns = np.array([1, 7, 5, 2, 14, 6, 9, 11])
-            # x = identity_matrix[:, selected_columns].T
-
-
+            # pass identity matrix to each layer for this pathway,
+            # assume each layers input and output dimens are fixed?
+            # iterate through pathway
             for op_list in self.hidden_layers:
                 xtemp = x
                 for op in op_list:
+                    # this return the weight matrix?
                     xtemp = op(xtemp)
+
                 output.append(xtemp.T.detach())
+
 
         return output
